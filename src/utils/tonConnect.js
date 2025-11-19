9123ac413013755bd40d43e3847274c033e43c21
SESHP <n.finnikov05@mail.ru>
2025-11-19 19:29:53 +0400

    update

--- src/utils/tonConnect.js
+++ src/utils/tonConnect.js
@@ -1,8 +1,8 @@
 
 export const tonConnectOptions = {
-//   manifestUrl: 'https://localhost:3000/tonconnect-manifest.json',
+  manifestUrl: 'https://alged.vercel.app/tonconnect-manifest.json',
   // Для разработки можно использовать localhost
 //   manifestUrl: 'http://localhost:3000/tonconnect-manifest.json'
-  manifestUrl: 'https://ton-connect.github.io/demo-dapp-with-react-ui/tonconnect-manifest.json'
+  // manifestUrl: 'https://ton-connect.github.io/demo-dapp-with-react-ui/tonconnect-manifest.json'
 
 };
